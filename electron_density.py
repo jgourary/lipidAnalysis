d1ca4d954ba595f9720fe3280f259d3f9b10938d
Vertimnus <jtgourary@gmail.com>
2021-11-10 10:37:40 -0600

    electron density

--- electron_density.py
+++ electron_density.py
@@ -4,18 +4,18 @@ import sys
 
 
 # Parameters
-verbose = False
+verbose = True
 num_bins = 100 # bin width = box height / num_bins
 
 
-def getAtomicNumbers(file):
-    prm = open(file, 'r')
+def getAtomicNumbers():
+    file = open(prm, 'r')
     masses = dict()
 
-    for line in prm:
+    for line in file:
         cleaned_line = line.rstrip()
         tokens = cleaned_line.split()
-        if tokens[0] == "atom":
+        if len(tokens) > 5 and tokens[0] == "atom":
             # remove atom name in quotes
             tokens = cleaned_line.split("\"")
             del(tokens[1])
@@ -26,11 +26,11 @@ def getAtomicNumbers(file):
             tokens = cleaned_line.split()
             if tokens[0] == "atom" and len(tokens) > 5:
                 masses[tokens[1]] = float(tokens[5])
-
+    print("Found atomic numbers for " + str(len(masses)) + " atom types")
     return masses
 
 
-def getFrameCount(file):
+def getFrameCount():
     arc = open(file, 'r')
 
     num_frames = 0
@@ -46,6 +46,8 @@ def getFrameCount(file):
         if cleaned_line == first_line:
             num_frames += 1
 
+    print("Found " + str(num_frames) + " frames in the archive")
+
     return num_frames
 
 
@@ -58,28 +60,43 @@ class Atom:
         self.bound_atoms = bound_atoms
 
 
-def get_box_dimensions(file):
-    xyz = open(file, 'r')
-    for line in xyz:
-        if "90.00000000   90.00000000   90.00000000" in line:
+class frameData:
+    def __init__(self, frame_count, electrons, densities):
+        self.frame_count = frame_count
+        self.electrons = electrons
+        self.densities = densities
+
+
+def get_box_dimensions():
+    arc = open(file, 'r')
+    i = 0
+    for line in arc:
+        if i == 1:
             tokens = line.split()
             return [float(tokens[0]), float(tokens[1]), float(tokens[2])]
+        i += 1
 
     return -1, -1, -1
 
 
-def get_bin_ranges(file):
-    dimensions = get_box_dimensions(file)
+def get_bin_ranges():
+    print("Finding bin cutoffs")
+    dimensions = get_box_dimensions()
+    print("Box Dimensions: [" + str(dimensions[0]) + ", " + str(dimensions[1]) + ", " + str(dimensions[2]) + "] Angstroms")
     bin_width = dimensions[2] / num_bins
+    print("Setting bin width to " + str(bin_width) + " Angstroms")
     lower_bound = dimensions[2] / -2
+    print("Lower bound is " + str(lower_bound) + " Angstroms")
     bin_cutoffs = np.empty((num_bins, 1))
     for i in range(0, num_bins):
         bin_cutoffs[i] = lower_bound + (i+1) * bin_width  # this is the upper cutoff
+
     bin_volume = bin_width * dimensions[0] * dimensions[1]
+    print("Setting bin volume to " + str(bin_volume) + " A^3")
     return bin_cutoffs, bin_volume
 
 
-def get_atom_bin(bin_cutoffs, z):
+def get_atom_bin(z):
     i = 0
     for bin_cutoff in bin_cutoffs:
         if z < bin_cutoff:
@@ -101,33 +118,46 @@ def return_atom(tokens):
     return newAtom
             
             
-def single_frame_electron_density(atoms):
-    single_frame_bins = np.empty((num_bins, 1))
-    
+def single_frame_electron_density(frame_num, atoms):
+    electrons = np.zeros((num_bins, 1))
+    densities = np.zeros((num_bins, 1))
+
+    # assign all atoms to relevant bins
+    if verbose:
+        print("Beginning atom bin assignment on frame " + str(frame_num))
     for atom_id in atoms:
+        #  if verbose:
+        #    print("Placing atom " + str(atoms[atom_id].num) + "...")
         i = 0
         for cutoff in bin_cutoffs:
             if atoms[atom_id].coords[2] < cutoff:
-                single_frame_bins[i] += atomic_numbers[atoms[atom_id].atom_type]
+                electrons[i] += atomic_numbers[atoms[atom_id].atom_type]
+                #if verbose:
+                #    print("Placing atom " + str(atoms[atom_id].num) + " in bin " + str(i))
+                break
+
             i += 1
+
+    #  convert bin electrons to bin electron density
     i = 0
-    for bin in single_frame_bins:
-        single_frame_bins[i] = bin / bin_volume
+    for elec_count in electrons:
+        densities[i] = elec_count / bin_volume
+        print(densities[i])
         i += 1
-    return single_frame_bins
+    newFrame = frameData(frame_num, electrons, densities)
+    return newFrame
 
 
 def electron_density(first_frame, last_frame):
     arc = open(file, 'r')
-    out = open(file, 'w')
-    out.write(bin_cutoffs)
+    out_file = open(out, 'w')
     # Get first line to identify splits between frames
     first_line = arc.readline().rstrip()
     
     # the number of frames we have read through
     frame_num = 0
 
-    average_bins = np.empty((num_bins, 1), dtype=float)
+    all_frame_data = []
     #  frames_to_process = last_frame - first_frame + 1
     atoms = {}
     
@@ -135,19 +165,17 @@ def electron_density(first_frame, last_frame):
     for line in arc:
         cleaned_line = line.rstrip()
         tokens = cleaned_line.split()
-        
+
         # indicates we have found a new frame
         if cleaned_line == first_line:
-
             # Process last frame
-            if 0 < frame_num <= last_frame and frame_num >= first_frame:
-                single_frame_bins = single_frame_electron_density(atoms)
-                out.write(single_frame_bins)
-                for i in (0, len(single_frame_bins)):
-                    average_bins[i] += single_frame_bins[i]
-
+            if verbose:
+                print("Found frame no. " + str(frame_num))
+            if 1 <= frame_num <= last_frame and frame_num >= first_frame:
                 if verbose:
-                    print("Frame Number: " + str(frame_num))
+                    print("Finding densities for frame " + str(frame_num))
+                this_frame_data = single_frame_electron_density(frame_num, atoms)
+                all_frame_data.append(this_frame_data)
 
             # iterate frame number counter
             frame_num += 1
@@ -160,22 +188,37 @@ def electron_density(first_frame, last_frame):
             
             newAtom = return_atom(tokens)
             atoms[newAtom.num] = newAtom
+    # Process last frame
+    this_frame_data = single_frame_electron_density(frame_num, atoms)
+    all_frame_data.append(this_frame_data)
 
-        for i in (0, len(average_bins)):
-            average_bins[i] = average_bins[i] / bin_volume
-            print("Bin Cutoff = " + str(bin_cutoffs[i]) + ", Density = " + str(average_bins[i]))
-    return average_bins
+    average_densities = np.zeros((num_bins, 1), dtype=frameData)
+    for frame in all_frame_data:
+        i = 0
+        for density in frame.densities:
+            print(density)
+            average_densities[i] += density
+            i += 1
+    i = 0
+    for density in average_densities:
+        average_densities[i] = density / num_frames
+        i += 1
+
+    i = 0
+    for cutoff in bin_cutoffs:
+        out_file.write(str(cutoff[0]) + ", " + str(average_densities[i,0]) + "\n")
+        i += 1
+
+    return average_densities
             
-
 
 file = sys.argv[1]
 ref = sys.argv[2]
 prm = sys.argv[3]
 out = sys.argv[4]
 
-bin_cutoffs, bin_volume = get_bin_ranges(file)
-atomic_numbers = getAtomicNumbers(prm)
-num_frames = getFrameCount(file)
-print("Num Frames = " + str(num_frames))
-bins = electron_density(file, 1, num_frames)
+bin_cutoffs, bin_volume = get_bin_ranges()
+atomic_numbers = getAtomicNumbers()
+num_frames = getFrameCount()
+bins = electron_density(1, num_frames)
 
